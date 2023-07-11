3d1978729ee2d55b2d84d6812169afc66bf48d48
bridgea13 <bri21025@byui.edu>
2023-07-11 14:53:15 -0400

    Scripture method

---
+++ prove/Develop3/Scripture.cs
@@ -0,0 +1,12 @@
+﻿
+
+public class Scripture
+{
+    public string Citation { get; set; }
+    public string Text { get; set; }
+    public Citation(string text, string citation)
+    {
+        Text = text;
+        Citation = citation;
+    }
+}
